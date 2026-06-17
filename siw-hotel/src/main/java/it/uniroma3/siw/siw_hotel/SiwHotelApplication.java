d95997e1258678cc96c1344e0b21b2f79e6451d1
Jojo23-Dev <villanueva.jonel0226@gmail.com>
2026-06-17 17:29:50 +0200

    base

---
+++ siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/SiwHotelApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwHotelApplication.class, args);
+	}
+
+}
---
+++ siw-hotel/src/test/java/it/uniroma3/siw/siw_hotel/SiwHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
