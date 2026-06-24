5303607eb0f60e215b87db8a0fe4f9498eb9aa1d
Jojo23-Dev <villanueva.jonel0226@gmail.com>
2026-06-24 18:20:11 +0200

    HTML
    creazione camera,cameraDettaglialo e home , controller, pom e properties

--- siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/controller/CameraController.java
+++ siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/controller/CameraController.java
@@ -1,6 +1,9 @@
 package it.uniroma3.siw.siw_hotel.controller;
 
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +15,30 @@ import it.uniroma3.siw.siw_hotel.service.CameraService;
 @Controller
 public class CameraController {
 
+    @Autowired
     private CameraService cameraService;
     
     @GetMapping("/camere/{id}")
     public String show(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("camera", this.cameraService.getCamera(id));
-    return "camere/show";
+        // 1. Recuperi l'Optional dal service
+        Optional<Camera> cameraOptional = cameraService.getCamera(id);
+    
+    // 2. CONTROLLA se esiste! 
+    if (cameraOptional.isPresent()) {
+        // 3. Estratto l'oggetto reale (Camera)
+        model.addAttribute("camera", cameraOptional.get());
+        return "camera"; // nome del tuo file HTML
+    } else {
+        // Gestisci il caso in cui l'id non esiste (es. redirect a errore o elenco)
+        return "redirect:/camere"; 
+    }
     }
 
     @GetMapping("/camere")
     public String list(Model model) {
-        Iterable<Camera> allMovies = this.cameraService.getCamere();
-        model.addAttribute("camere", allMovies);
-    return "camere/list";
+        Iterable<Camera> allCamere = this.cameraService.getCamere();
+        model.addAttribute("camere", allCamere);
+    return "camere";
     }
 
     
--- siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/controller/HomeController.java
+++ siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
     
 	@GetMapping("/")
 		public String getHome() {
-		return "index.html";
+		return "home.html";
 	}
 
 }
