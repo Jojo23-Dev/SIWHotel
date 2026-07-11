8c8fb8ee0dab0a54d28a71e5015d1b55ffa4a619
michelemoscarelli <166754020+michelemoscarelli@users.noreply.github.com>
2026-07-11 21:17:06 +0200

    interfaccia admin
    create le pagine riservate agli admin: dashboard, elenco prenotazioni ricevute, anagrafica clienti, pagina camere in modalità amministratore (con aggiungi, modifica ed elimina). implementare le funzioni in back-end (anche la cancellazione da parte dell'admin di una prenotazione dove mancano meno di 3 giorni, probabilmente è dovuto al metodo dell'operazione in prenotazionecontroller che non faceva cancellare mai una prenotazione fuori da quel tempo massimo. aggiungere invece la possibilità di farlo da ruolo admin

---
+++ siw-hotel/src/main/java/it/uniroma3/siw/siw_hotel/controller/AdminController.java
@@ -0,0 +1,39 @@
+package it.uniroma3.siw.siw_hotel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.uniroma3.siw.siw_hotel.service.PrenotazioneService;
+import it.uniroma3.siw.siw_hotel.service.UtenteService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private PrenotazioneService prenotazioneService;
+
+    @Autowired
+    private UtenteService utenteService;
+
+    @GetMapping("/dashboard")
+    public String mostraDashboard() {
+        return "admin/dashboard";
+    }
+
+    @GetMapping("/prenotazioni")
+    public String elencoPrenotazioni(Model model) {
+        model.addAttribute("prenotazioni", this.prenotazioneService.getPrenotazioniTutte()); 
+        return "admin/prenotazioni";
+    }
+
+    @GetMapping("/clienti")
+    public String elencoClienti(Model model) {
+        model.addAttribute("clienti", this.utenteService.getUtenti()); 
+        return "admin/clienti";
+    }
+
+}
